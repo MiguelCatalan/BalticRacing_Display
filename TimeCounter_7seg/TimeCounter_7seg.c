6cf5456183684777ac73b1d5653210714961c691
Miguel Catalan <miguel.catalan.banuls@gmail.com>
2014-10-07 00:37:14 +0200

    Included libraries into the main program

--- TimeCounter_7seg/TimeCounter_7seg.c
+++ TimeCounter_7seg/TimeCounter_7seg.c
@@ -1,13 +1,17 @@
 /*
  * TimeCounter_7seg.c
  *
- * Created: 06/10/2014 22:47:49
+ *	Created: 06/10/2014 22:47:49
  *  Author: Miguel Catalan Bañuls
  *  Team: Baltic Racing
  */ 
 
-
+#define F_CPU	4000000UL
 #include <avr/io.h>
+
+#include "Display.h"
+#include "Memory.h"
+#include "Time.h"
 
 int main(void)
 {
