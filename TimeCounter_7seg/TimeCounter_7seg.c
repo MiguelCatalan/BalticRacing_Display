980752f7cef72b6d6e5f890cda7d31523097d89b
Miguel Catalan <miguel.catalan.banuls@gmail.com>
2014-10-06 23:05:07 +0200

    Added base project

---
+++ TimeCounter_7seg/TimeCounter_7seg.c
@@ -0,0 +1,18 @@
+/*
+ * TimeCounter_7seg.c
+ *
+ * Created: 06/10/2014 22:47:49
+ *  Author: Miguel Catalan Bañuls
+ *  Team: Baltic Racing
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
