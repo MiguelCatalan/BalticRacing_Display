861870c4228588f89e6f6ac7cde97ab65ec90051
Miguel Catalan <miguel.catalan.banuls@gmail.com>
2014-10-07 00:43:36 +0200

    Added some declarations to the main program

--- TimeCounter_7seg/TimeCounter_7seg.c
+++ TimeCounter_7seg/TimeCounter_7seg.c
@@ -13,10 +13,25 @@
 #include "Memory.h"
 #include "Time.h"
 
+#define SegmentDataPort		PORTB //TODO: just for testing, find the right port
+#define SegmentDataPin		PINB
+#define SegmentDataDDR		DDRB
+
+#define SegmentControlPort		PORTC
+#define SegmentControlPin		PINC
+#define SegmentControlDDR		DDRC
+
+/*
+ *	Global Variables Declarations
+ */
+unsigned char hours = 0;
+unsigned char minutes = 0;
+unsigned char seconds = 0;
+
 int main(void)
 {
     while(1)
     {
-        //TODO:: Please write your application code 
+		//TODO: Do my magic   
     }
 }
