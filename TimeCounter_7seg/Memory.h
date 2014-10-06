37fb8700145ab934ad6185a1d8a115a6f44d3dde
Miguel Catalan <miguel.catalan.banuls@gmail.com>
2014-10-07 00:04:17 +0200

    Added prototype of the Memory Library

---
+++ TimeCounter_7seg/Memory.h
@@ -0,0 +1,19 @@
+/*
+ *	Manage the Memory read and write methods
+ *
+ *	Created: 06/10/2014 22:47:49
+ *  Author: Miguel Catalan Bañuls
+ *  Team: Baltic Racing
+ */ 
+
+#ifndef Memory
+#define Memory
+
+void read(){
+	
+}
+
+void write(){
+	
+}
+#endif
